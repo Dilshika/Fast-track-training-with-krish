1f58b4bb64d391d962c35b336bd8af9a157782fe
Dilshika <dilshika.bandara@gmail.com>
2021-05-29 06:51:42 +0530

    selection sort implementation

---
+++ sortingAlgorithms/selectionSort/selectionsort.js
@@ -0,0 +1,31 @@
+module.exports = function selectionSort(array) {
+
+    let size = array.length,
+        min, i, j;
+
+    for (i = 0; i < size; i++) {
+
+        min = i;
+
+        // check for the minimum
+        for (j = i + 1; j < size; j++) {
+            if (array[j] < array[min]) {
+                min = j;
+            }
+        }
+
+        // if the minimum isn't in the position, swap it
+        if (i != min) {
+            swap(array, i, min);
+        }
+    }
+
+    return array;
+}
+
+
+function swap(array, firstIndex, secondIndex) {
+    let temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+}
